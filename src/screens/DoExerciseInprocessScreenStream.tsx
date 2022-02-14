d82847e7397a15428de1fd6f500f09ac22e1caf4
Khánh Pluto <47073675+khanh41@users.noreply.github.com>
2022-02-15 00:50:37 +0700

    🌟 Add do exercise inprocess screen stream (#8)

---
+++ src/screens/DoExerciseInprocessScreenStream.tsx
@@ -0,0 +1,65 @@
+import { Camera } from "expo-camera";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text } from "../components/Themed";
+import { RootStackScreenProps } from "../types/types";
+
+export default function DoExerciseInprocessScreenStream({
+  route,
+  navigation,
+}: RootStackScreenProps<"DoExerciseInprocessStream">) {
+  console.log(route.params["name"]);
+  const [hasPermission, setHasPermission] = useState<boolean>();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={Camera.Constants.Type.front}
+        ratio={"16:9"}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
