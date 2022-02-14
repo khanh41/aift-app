6607b767cafdc5ca0f0e45578d89756ee71c66fe
Khánh Pluto <47073675+khanh41@users.noreply.github.com>
2022-02-15 00:46:08 +0700

    🏓 Refactor project and update dependency (#5)

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,3 +1,6 @@
 export const replaceText = "xx123xx";
 
 export const firebaseImageUrl = `https://firebasestorage.googleapis.com/v0/b/aift-b7b2c.appspot.com/o/${replaceText}.jpg?alt=media`;
+
+// export const apiUrl = "http://192.168.1.52:8088/api";
+export const apiUrl = "http://aift.southeastasia.cloudapp.azure.com:8088/api";
--- src/types/UserType.ts
+++
@@ -1,7 +0,0 @@
-export default interface UserType {
-  id?: any | null;
-  username?: string | null;
-  email?: string;
-  password?: string;
-  roles?: Array<string>;
-}
