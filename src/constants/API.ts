0fa0a47f37d9027a4f1903ffcacbcdbb071e62af
Khánh Pluto <tranvankhanh99a@gmail.com>
2022-02-15 22:52:23 +0700

    Video screen and call api predict

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -2,5 +2,7 @@ export const replaceText = "xx123xx";
 
 export const firebaseImageUrl = `https://firebasestorage.googleapis.com/v0/b/aift-b7b2c.appspot.com/o/${replaceText}.jpg?alt=media`;
 
-// export const apiUrl = "http://192.168.1.52:8088/api";
-export const apiUrl = "http://aift.southeastasia.cloudapp.azure.com:8088/api";
+// const uri = 'http://192.168.43.90:8088'
+const uri = 'http://aift.southeastasia.cloudapp.azure.com:8088'
+export const apiUrl = uri + "/api";
+export const videoUrl = uri + "/video/";
